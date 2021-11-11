a0317e4102534bef14d15922a364303d2cf169fb
Your Name <you@example.com>
2021-11-11 23:53:18 +0700

    giai pt bac 1

---
+++ ptbac1.js
@@ -0,0 +1,15 @@
+function giai_pt_bac_nhat(a, b)
+{
+    if (a == 0 && b == 0){
+        alert('Phương trình vô số nghiệm');
+    }
+    else if (a != 0 && b == 0){
+        alert('Phương trình có nghiệm x = 0');
+    }
+    else if (a == 0 && b != 0){
+        alert("Phương trình vô nghiệm");
+    }
+    else {
+        alert('Phương trình có nghiệm x = ' + (-b/a));
+    }
+}
